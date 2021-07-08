0afb33b52101e5a01062d2d50ed065967c37f2f2
Bhargav <bhargavkr2000@gmail.com>
2021-07-08 23:48:35 +0530

    important concepts in javascript

---
+++ script.js
@@ -0,0 +1,34 @@
+/**************************************Ternary Operator******************************************** */
+console.log("teranary operator");
+let a = 5;
+let b = 6;             
+(a > b)? console.log(a+" is greater"): console.log(b+" is greater");
+console.log("*******************")
+/**************************************(Nullish Operator ??)******************************************** */
+let user;
+let myName = 'ramesh';
+console.log(myName ?? "name not initialized");
+console.log(user ?? "not Defined");
+console.log("******************");
+/**********************************************Call Back Functions********************************************** */
+//console.log(confirm("dog is an animal do you agree?"));
+
+function ask(question, aggreed, notaggreed){
+    if(confirm(question)){
+        aggreed()
+    }
+    else{
+        notaggreed()
+    }
+}
+
+function showOk(){
+    console.log("i know yor are a human");
+}
+
+function showCancel(){
+    console.log("Don't tell me lies baka...");
+}
+
+ask("are you human?",showOk,showCancel);
+/****************************************************************************************************** */
