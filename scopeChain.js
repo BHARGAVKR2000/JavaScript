0e265c27ef2426fee8e79f4e53ca82978a1c8d55
Bhargav <bhargavkr2000@gmail.com>
2021-07-09 18:17:01 +0530

    lexical environment

--- scopeChain.js
+++ scopeChain.js
@@ -1,6 +1,12 @@
-let a;
-console.log(a);
-a = 5;
-console.log(a);
-a = 'samantha';
-console.log(a);
+function add(){
+    let result = a + b;     // a and b are accessable even inside the function
+    display();
+    function display(){         // display is lexically inside add function
+        console.log(result);
+    }
+}
+
+let a = 5;
+let b = 5;
+
+add();
