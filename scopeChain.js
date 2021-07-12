608a279078b9cb611fc9692801d933643625f0de
Bhargav <bhargavkr2000@gmail.com>
2021-07-12 12:42:54 +0530

    ScopeChain in js

--- scopeChain.js
+++ scopeChain.js
@@ -1,12 +1,29 @@
-function add(){
-    let result = a + b;     // a and b are accessable even inside the function
-    display();
-    function display(){         // display is lexically inside add function
-        console.log(result);
-    }
+function add() {
+  let result = a + b; // a and b are accessable even inside the function
+  display();
+  function display() {
+    // display is lexically inside add function
+    console.log(result);
+  }
 }
 
-let a = 5;
+console.log(a);
+
+var a = 5;
 let b = 5;
 
+console.log(b);
+
 add();
+var a = "ramesh"; // var can be rediclared but let cannot
+console.log(a);
+ 
+if(true){
+    var age = 30;
+    let id = 373;
+    console.log(id);
+    console.log(age);
+}
+
+console.log("outside the block scope  age:"+age);
+
