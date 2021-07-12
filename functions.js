3fef0dcec404692a5dc782600f52ba5ce4fe4356
Bhargav <bhargavkr2000@gmail.com>
2021-07-12 12:37:40 +0530

    fuctions in javascript

---
+++ functions.js
@@ -0,0 +1,25 @@
+//fuction statement and function declaration(The way of creating a function as shown below is called function statement)
+statement();
+function statement(){
+    console.log("a function statement..");
+}
+
+//fuction Experession
+let func = function(){
+    console.log("function expression..");
+}
+func();
+
+//Anonymous function( a function without a function name)
+var a = () => {
+    console.log("anonymus function")
+}
+
+a();
+
+//Named Function Expression
+let func1 = function name(){
+    console.log("Named Function..");
+}
+
+
